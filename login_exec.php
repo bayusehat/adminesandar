cf50bcea4725bc9f9bad704f317a075a653f778b
Arya Bayu <aryabayu23@gmail.com>
2019-04-26 21:57:05 +0700

    initial commit

---
+++ login_exec.php
@@ -0,0 +1,28 @@
+<?php session_start();
+
+include "config.php";
+require_once("function_pass.php");
+
+$username=$_POST['namauser'];
+$pwd=$_POST['password'];
+$query=mysqli_query($connect,"select * from login where username='$username' and password='$pwd'");
+$cek=mysqli_num_rows($query);
+$hasil=mysqli_fetch_array($query);
+
+
+if($cek > 0){
+$_SESSION['id_akses']=$hasil['id_akses'];
+$_SESSION['namauser']=$username;
+$_SESSION['nama_lengkap']=$hasil['nama_lengkap'];
+$_SESSION['hak_akses']=$hasil['hak_akses'];
+echo "<script>document.location='index.php';</script>";
+}else{
+	echo "<script>;
+		alert('Username/Password salah');
+		window.location='login.php';</script>";
+	echo mysqli_error($connect);
+}
+
+
+
+?>
